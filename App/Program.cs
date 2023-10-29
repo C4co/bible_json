46dce3f525ef4a6b57192ab9a683e75dc444c633
Carlos Costa <carllos.nc@gmail.com>
2023-10-29 14:45:41 -0300

    add: support to get bibles as .json and .gzip

--- App/Program.cs
+++ App/Program.cs
@@ -1,78 +1,155 @@
 ﻿using BibleJson.Services;
 using BibleJson.Domain;
 using System.Text.Json;
+using System.IO.Compression;
 
-class Program
+namespace App
 {
-    static public async Task Process(
-        string url,
-        string lang,
-        string fileName
-    )
+    public enum OutputType
     {
-        BibleService bibleService = new(
-            new HttpClient()
-        );
+        json,
+        gzip
+    }
 
-        List<Book> oldTestamentBooks = await bibleService.GetBooks(
-            BookType.OldTestament,
-            url
-        );
-
-        List<Book> newTestamentBooks = await bibleService.GetBooks(
-            BookType.NewTestament,
-            url
-        );
-
-        Bible bible = new(
-            books: oldTestamentBooks.Concat(newTestamentBooks).ToList(),
-            lang: lang
-        );
-
-        foreach (var book in bible.Books)
+    class Program
+    {
+        static public async Task Process(
+            string url,
+            string lang,
+            string fileName,
+            OutputType outputType = OutputType.gzip
+        )
         {
-            var numberOfChapters = await bibleService.GetNumberOfChapters(book);
-            book.NumberOfChapters = numberOfChapters;
+            BibleService bibleService = new(
+                new HttpClient(),
+                lang
+            );
 
-            for (int i = 1; i <= numberOfChapters; i++)
+            List<Book> oldTestamentBooks = await bibleService.GetBooks(
+                BookType.OldTestament,
+                url
+            );
+
+            List<Book> newTestamentBooks = await bibleService.GetBooks(
+                BookType.NewTestament,
+                url
+            );
+
+            Bible bible = new(
+                books: oldTestamentBooks.Concat(newTestamentBooks).ToList(),
+                lang: lang
+            );
+
+            foreach (var book in bible.Books)
             {
-                var chapter = await bibleService.GetVerses(book, i);
+                var numberOfChapters = await bibleService.GetNumberOfChapters(book);
+                book.NumberOfChapters = numberOfChapters;
 
-                Console.WriteLine($"Processing... {book.Name} Chapter: {i}");
+                for (int i = 1; i <= numberOfChapters; i++)
+                {
+                    var chapter = await bibleService.GetVerses(book, i);
 
-                book.Chapters?.Add(chapter);
+                    Console.WriteLine($"Processing... {book.Name} Chapter: {i}");
+
+                    book.Chapters?.Add(chapter);
+                }
+            }
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+
+            var json = JsonSerializer.Serialize(bible, options);
+
+            if (outputType == OutputType.json)
+            {
+                File.WriteAllText($"../books/{fileName}.json", json);
+                return;
+            }
+
+            if (outputType == OutputType.gzip)
+            {
+                using var fileStream = File.Create($"../books/{fileName}.gz");
+                using var gzipStream = new GZipStream(fileStream, CompressionMode.Compress);
+                using var writer = new StreamWriter(gzipStream);
+                await writer.WriteAsync(json);
+                return;
             }
         }
 
-        var options = new JsonSerializerOptions
+        public static async Task Main(string[] args)
         {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            WriteIndented = true
-        };
+            //Gzip
 
-        var json = JsonSerializer.Serialize(bible, options);
+            // await Process(
+            //     url: "https://www.bibliaonline.com.br/acf/livros",
+            //     lang: "pt-br",
+            //     fileName: "bible_pt-br"
+            // );
 
-        File.WriteAllText($"../books/{fileName}", json);
-    }
+            // await Process(
+            //     url: "https://www.bibliaonline.com.br/acv/livros",
+            //     lang: "en",
+            //     fileName: "bible_en"
+            // );
 
-    public static async Task Main(string[] args)
-    {
-        await Process(
-            url: "https://www.bibliaonline.com.br/acf/livros",
-            lang: "pt-br",
-            fileName: "bible_pt-br.json"
-        );
+            // await Process(
+            //     url: "https://www.bibliaonline.com.br/bm1844/livros",
+            //     lang: "fr",
+            //     fileName: "bible_fr"
+            // );
 
-        await Process(
-            url: "https://www.bibliaonline.com.br/acv/livros",
-            lang: "en",
-            fileName: "bible_en.json"
-        );
+            // await Process(
+            //     url: "https://www.bibliaonline.com.br/itadio/livros",
+            //     lang: "it",
+            //     fileName: "bible_it"
+            // );
 
-        await Process(
-            url: "https://www.bibliaonline.com.br/bm1844/livros",
-            lang: "fr",
-            fileName: "bible_fr.json"
-        );
+            // await Process(
+            //     url: "https://www.bibliaonline.com.br/sev/livros",
+            //     lang: "es",
+            //     fileName: "bible_es"
+            // );
+
+            // Json
+
+            await Process(
+                url: "https://www.bibliaonline.com.br/acf/livros",
+                lang: "pt-br",
+                fileName: "bible_pt-br",
+                outputType: OutputType.json
+            );
+
+            await Process(
+                url: "https://www.bibliaonline.com.br/acv/livros",
+                lang: "en",
+                fileName: "bible_en",
+                outputType: OutputType.json
+            );
+
+            await Process(
+                url: "https://www.bibliaonline.com.br/bm1844/livros",
+                lang: "fr",
+                fileName: "bible_fr",
+                outputType: OutputType.json
+            );
+
+            await Process(
+                url: "https://www.bibliaonline.com.br/itadio/livros",
+                lang: "it",
+                fileName: "bible_it",
+                outputType: OutputType.json
+            );
+
+            await Process(
+                url: "https://www.bibliaonline.com.br/sev/livros",
+                lang: "es",
+                fileName: "bible_es",
+                outputType: OutputType.json
+            );
+        }
     }
 }
